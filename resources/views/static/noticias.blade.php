8b615bd9829982195c02775b5ad1614819d2d6f9
MarcoAMtz99 <marcoamtz99@gmail.com>
2021-03-19 11:18:45 -0600

    Cambios a gnp

--- resources/views/static/noticias.blade.php
+++ resources/views/static/noticias.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.app')
 @section('content')
 <div class="row justify-content-center">
-	<div class="col-10 col-sm-6 my-5 mx-0 p-0 bg-light rounded shadow-lg">
+	<div class="col-12 col-sm-8 my-5 mx-0 p-0 bg-light rounded shadow-lg">
         <div class="tab-content" id="myTabContent">
             <div class="tab-pane fade show active">
                 <div class="card card-default">
@@ -10,27 +10,26 @@
                             Noticias
                         </h3>
                     </div>
-                 <!--    <div class="card-body">
-                        <p class="card-text">
-                            <strong>Autosegurodirecto.com</strong> es el sitio web que pone a disposición de todos la forma más eficiente de:
-                        </p>
-                        <dl class="ml-3">
-                            <dt><li>Cotizar</li></dt>
-                            <dt><li>Comparar e informarse de los detalles de cada propuesta de aseguramiento</li></dt>
-                            <dt><li>Contratar</li></dt>
-                            <dt><li>Pagar</li></dt>
-                        </dl>
-                        <p class="card-text">
-                            Tu <strong>póliza de seguro de auto</strong> con las siguientes ventajas:
-                        </p>
-                        <dl class="ml-3">
-                            <dt><li>Los precios más competitivos</li></dt>
-                            <dt><li>Facilidad de Contratar</li></dt>
-                            <dt><li>Facilidad de pago</li></dt>
-                            <dt><li>Seguridad</li></dt>
-                            <dt><li>Comodidad y simplicidad</li></dt>
-                        </dl>
-                    </div> -->
+                 <div class="faq_container ml-5">
+                                                   <div class="faq">
+                                                      <div class="faq_question"> NOTICIA 1</div>
+                                                           <div class="faq_answer_container">
+                                                              <div class="faq_answer mr-5">Paquete ROBO TOTAL
+                                                                Es muy similar al paquete AMPLIO con la diferencia de que no incluye los “Daños Materiales” del contratante que se le lleguen a causar por motivo de un accidente automovilístico. (ver tabla).
+                                                                </div>
+                                                           </div>        
+                                                    </div>
+                                                 </div>
+
+
+
+
+
+
+
+
+
+
                 </div>
             </div>
         </div>
