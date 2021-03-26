fd6b746edb97c2d4eb00dd32dbcd7d5562412e18
MarcoAMtz99 <marcoamtz99@gmail.com>
2021-03-26 02:37:28 -0600

    Cambios en GNP

--- app/Http/Controllers/GNPController.php
+++ app/Http/Controllers/GNPController.php
@@ -746,7 +746,7 @@ class GNPController extends Controller
    //  	array('xml' =>$data , 'request' => $array_data));
 
 			// dd($data,$this->curl->response);
-			 dd($data,$array_data); 
+			 // dd($data,$array_data); 
 	        return view('gnp.poliza',['response'=>$array_data ,'data'=>$data]);
 	        // return response()->json(['cotizacionGNP'=>$array_data],201);
 		} catch (Exception $e) {
