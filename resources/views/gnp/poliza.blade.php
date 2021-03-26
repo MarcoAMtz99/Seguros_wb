5c36c1d791815ec18b9f778b95ba9cb0006f50f0
MarcoAMtz99 <marcoamtz99@gmail.com>
2021-03-26 03:36:10 -0600

    Cambios en GNP

--- resources/views/gnp/poliza.blade.php
+++ resources/views/gnp/poliza.blade.php
@@ -58,7 +58,7 @@
 						@endif
 					</div>
 					@endif
-					{{dd($response)}}
+					<!-- {{dd($response)}} -->
 					@if(isset($response['CONCEPTOS_ECONOMICOS']))
 					<div class="col-12">
 						<div class="text-center mt-4">
