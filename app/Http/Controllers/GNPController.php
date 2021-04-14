67d27994b39afe88560f9998421c87f66dbab176
MarcoAMtz99 <marcoamtz99@gmail.com>
2021-04-14 18:13:02 -0500

    Cambio en GNP PRUEBAS

--- app/Http/Controllers/GNPController.php
+++ app/Http/Controllers/GNPController.php
@@ -200,7 +200,7 @@ class GNPController extends Controller
  		$json_mod = json_decode($Modelos);
 
  		// dd($json_mod->ELEMENTOS);
- 		foreach ($json_mod->ELEMENTOS as $object) {
+ 		foreach ($json_mod->ELEMENTOS->ELEMENTO as $object) {
  			# code...
  			print_r($object->NOMBRE);
  		}
