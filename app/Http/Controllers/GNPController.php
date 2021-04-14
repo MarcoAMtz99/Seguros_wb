b280e6a256143f5c416975fa685df126f6fd14f1
MarcoAMtz99 <marcoamtz99@gmail.com>
2021-04-14 18:40:22 -0500

    Cambio en GNP PRUEBAS

--- app/Http/Controllers/GNPController.php
+++ app/Http/Controllers/GNPController.php
@@ -110,7 +110,7 @@ class GNPController extends Controller
    //  		<NOMBRE>ARMADORA</NOMBRE>
    //  		<CLAVE>CH</CLAVE>
    //  		</ELEMENTO>
-				$modelos = $this->BusquedaMedelo('2019', 'CROSSFORD');
+				$modelos = $this->BusquedaMedelo('2019', 'FIESTA');
 				// $carroceria = $this->getCarroceria("HO", "CIVIC");
 				// $modelos    = $this->getModelos($modelo, $armadora, $carroceria);
 				dd($modelos);
