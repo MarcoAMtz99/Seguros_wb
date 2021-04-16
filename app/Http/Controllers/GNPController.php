976f2f7b3e4a4346d29e4aeaf0f8c64f39e622da
MarcoAMtz99 <marcoamtz99@gmail.com>
2021-04-16 12:33:44 -0500

    Cambios GNP

--- app/Http/Controllers/GNPController.php
+++ app/Http/Controllers/GNPController.php
@@ -110,7 +110,7 @@ class GNPController extends Controller
    //  		<NOMBRE>ARMADORA</NOMBRE>
    //  		<CLAVE>CH</CLAVE>
    //  		</ELEMENTO>
-				$modelos = $this->BusquedaModelo('2017', 'C-HR');
+				$modelos = $this->BusquedaModelo('2017', 'TOYOTA C-HR');
 				// $carroceria = $this->getCarroceria("HO", "CIVIC");
 				// $modelos    = $this->getModelos($modelo, $armadora, $carroceria);
 				dd($modelos);
