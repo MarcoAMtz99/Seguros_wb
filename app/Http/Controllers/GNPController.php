a09ea6e5cbe6aae0800fd122bc735ca54ac7d197
MarcoAMtz99 <marcoamtz99@gmail.com>
2021-04-27 16:20:19 -0500

    Prueba general de seguros

--- app/Http/Controllers/GNPController.php
+++ app/Http/Controllers/GNPController.php
@@ -110,7 +110,7 @@ class GNPController extends Controller
    //  		<NOMBRE>ARMADORA</NOMBRE>
    //  		<CLAVE>CH</CLAVE>
    //  		</ELEMENTO>
-				$modelos = $this->BusquedaModelo('2015', 'SPORTAGE');
+				$modelos = $this->BusquedaModelo('2015 	', 'SERIE 301');
 				// $carroceria = $this->getCarroceria("HO", "CIVIC");
 				// $modelos    = $this->getModelos($modelo, $armadora, $carroceria);
 				dd($modelos);
