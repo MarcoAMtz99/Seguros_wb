649b50ec1ae98feb269b5989f25537c9d82dfd86
MarcoAMtz99 <marcoamtz99@gmail.com>
2021-05-28 08:12:44 -0500

    Cambios en la mascarilla de GNP

--- app/Http/Controllers/GeneralSegurosController.php
+++ app/Http/Controllers/GeneralSegurosController.php
@@ -281,7 +281,7 @@ class GeneralSegurosController extends Controller
     {
          $versiones = [];
         $marca_gs = $this->searchMarca($marca);
-        // dd($marca_gs);
+        dd($marca_gs);
         if ($marca_gs) {
             $submarca_gs = $this->searchSubMarca($marca_gs, $submarca);
             dd($submarca_gs,$marca_gs, $submarca);
