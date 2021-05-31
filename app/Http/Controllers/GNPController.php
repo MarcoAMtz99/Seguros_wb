ddc1aa91223ef4ed47859b56f536c47c38996436
MarcoAMtz99 <marcoamtz99@gmail.com>
2021-05-31 12:28:02 -0500

    Cambio en la impresion de la poliza

--- app/Http/Controllers/GNPController.php
+++ app/Http/Controllers/GNPController.php
@@ -146,7 +146,7 @@ class GNPController extends Controller
 		 // $this->curl->post("https://api.service.gnp.com.mx/autos/wsp/catalogos/catalogo", $xml);
 
 
-			$this->curl->post("https://api.service.gnp.com.mx/autos/wsp/impresion/buscarPoliza", $xml_impre);
+			$this->curl->post("https://api.service.gnp.com.mx/autos/wsp/impresion/buscarPoliza", $xml);
 	        //convert the XML result into array
 	        // $array_data = json_decode(json_encode(simplexml_load_string($this->curl->response)), true);
 	        $array_data = json_decode(json_encode(simplexml_load_string($this->curl->response)), true);
