ee2392393358ff1bf1dfaa25716257f360267334
MarcoAMtz99 <marcoamtz99@gmail.com>
2021-06-01 18:38:48 -0500

    Plantilla de documentos email

--- app/Services/Ana/EmitirPolizaService.php
+++ app/Services/Ana/EmitirPolizaService.php
@@ -24,8 +24,8 @@ class EmitirPolizaService
         $this->setParams($request);
         $this->url = "https://server.anaseguros.com.mx/ananetws/service.asmx?wsdl";
         $this->urlPHP = "https://server.anaseguros.com.mx/ananetws/servicetext.asmx?wsdl";
-        $correo_e = new EmisionPoliza($request);
-        Mail::to('chirris4000@gmail.com')->send(new EmisionPoliza($request));
+        // $correo_e = new EmisionPoliza($request);
+        // Mail::to('chirris4000@gmail.com')->send(new EmisionPoliza($request));
         // dd($request->all());
         $estado = str_pad($request->estado, 2, "0", STR_PAD_LEFT);
         $municipio = str_pad($request->municipio_id, 3, "0", STR_PAD_LEFT);
