26343428830e401785a15ff367304a54f446d2d2
MarcoAMtz99 <marcoamtz99@gmail.com>
2021-06-03 18:24:45 -0500

    Pruebas GNP

--- app/Http/Controllers/GNPController.php
+++ app/Http/Controllers/GNPController.php
@@ -219,7 +219,7 @@ $xml ='
 			
 				$this->curl->post("https://api.service.gnp.com.mx/autos/wsp/impresion/buscarPoliza",$xml);
  				$array_data = json_decode($this->curl->response);
-	        // dd($data,$array_data);
+	        dd($xml,$array_data);
 	        return $array_data;
 		} catch (Exception $e) {
 
