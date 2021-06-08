61e4be4ea1cb397f037d5d52dd02fec8c2b81375
MarcoAMtz99 <marcoamtz99@gmail.com>
2021-06-08 15:45:17 -0500

    Cambios en la plantilla con error

--- app/Http/Controllers/GNPController.php
+++ app/Http/Controllers/GNPController.php
@@ -1197,7 +1197,7 @@ $xml ='
                 		 	 Mail::to($request->correo)->send(new EmisionPoliza($pdf,'GNP'));
 	        				return view('gnp.poliza',['response'=>$array_data ,'data'=>$data,'pdf'=>$pdf]);
 					}else{
-							return redirect()->back()->withErrors(['msg'=>[$request->cliente],'nombre'=>[$datos->nombre] ]);
+							return redirect()->back()->withErrors(['msg'=>[$request->cliente],'nombre'=>[$request->nombre] ]);
 					}
 
          //     		$num_poliza =  $array_data['SOLICITUD']["NUM_POLIZA"];
