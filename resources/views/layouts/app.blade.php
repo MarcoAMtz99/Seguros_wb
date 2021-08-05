bdb86634d0f75217837a12aa46c35ff99fa1e9bb
MarcoAMtz99 <marcoamtz99@gmail.com>
2021-08-04 20:34:20 -0500

    Cambios en controllador

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -233,7 +233,7 @@
          <script src="{{mix('js/app.js')}}"></script>
         <script src="{{ asset('js/modernizr-custom.js') }}"></script>
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.bundle.min.js" integrity="sha384-b5kHyXgcpbZJO/tY9Ul7kGkf1S0CWuKcCD38l8YkeH8z8QjE0GmW1gYU5S9FOnJ0" crossorigin="anonymous"></script>
-            <script type="text/javascript" charset="utf8" src="/DataTables/datatables.js"></script>
+         <script type="text/javascript" charset="utf8" src="https://cdn.datatables.net/1.10.25/js/jquery.dataTables.js"></script>
         <script src="https://ajax.googleapis.com/ajax/libs/jqueryui/1.12.1/jquery-ui.min.js"></script>
         <script>
             $('.botonF1').hover(function(){
