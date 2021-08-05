02ace228739719d7b133e7de87a5142f82162dde
MarcoAMtz99 <marcoamtz99@gmail.com>
2021-08-04 20:06:14 -0500

    Cambios en controllador

--- resources/views/prueba.blade.php
+++ resources/views/prueba.blade.php
@@ -11,7 +11,24 @@
 			</form>		
 
 			@if(isset($modelos))
-				{{dd($modelos)}}
+				{{dd($modelos['ELEMENTOS']['ELEMENTO'] )}}
+				<table style="width:100%">
+						  <tr>
+						    <th>UNO </th>
+						    <th>DOS</th>
+						    <th>TRES</th>
+						  </tr>
+						  <tr>
+						    <td>Jill</td>
+						    <td>Smith</td>
+						    <td>50</td>
+						  </tr>
+						  <tr>
+						    <td>Eve</td>
+						    <td>Jackson</td>
+						    <td>94</td>
+						  </tr>
+						</table>
 			@endif
 
 
