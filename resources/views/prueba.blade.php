8093f9e71824c7d288b41c129a8d70fe602bbc3a
MarcoAMtz99 <marcoamtz99@gmail.com>
2021-08-09 16:32:35 -0500

    Cabio en controlador GNP

--- resources/views/prueba.blade.php
+++ resources/views/prueba.blade.php
@@ -23,7 +23,7 @@
 						      <th>CARROCERIA</th>
 						       <th>VERSION</th>
 						  </tr>
-						 
+						 <h1>MODELOS EN COINCIDENCIA</h1>
 						@foreach($modelos["ELEMENTOS"] as $key)
 						@if($key["ELEMENTO"][3]["VALOR"] === $submarca)
 						 <tr>
@@ -33,9 +33,33 @@
 							    <td>{{$key["ELEMENTO"][4]["VALOR"]}} </td>
 
 							</tr>
+
 							@endif
 							@endforeach
-						  
+					
+						</table>
+						<hr>
+							<table id="table_id" style="width:100% ; background: white;">
+						  <tr>
+						   
+						    <th>ARMADORA</th>
+						    <th>MODELO</th>
+						      <th>CARROCERIA</th>
+						       <th>VERSION</th>
+						  </tr>
+						 <h1>MODELOS  completos </h1>
+						@foreach($modelos["ELEMENTOS"] as $key)
+					
+						 <tr>
+							 <td>{{$key["ELEMENTO"][1]["VALOR"]}} </td>
+							  <td>{{$key["ELEMENTO"][2]["VALOR"]}} </td>
+							   <td>{{$key["ELEMENTO"][3]["VALOR"]}} </td>
+							    <td>{{$key["ELEMENTO"][4]["VALOR"]}} </td>
+
+							</tr>
+
+							
+							@endforeach
 					
 						</table>
 			@endif
