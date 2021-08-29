29834f234d3b1dadec2d01a094a072b900868c76
MarcoAMtz99 <marcoamtz99@gmail.com>
2021-08-28 21:53:18 -0500

    Prueba en ana cotizacion

--- app/Http/Controllers/GeneralSegurosController.php
+++ app/Http/Controllers/GeneralSegurosController.php
@@ -310,10 +310,10 @@ class GeneralSegurosController extends Controller
             if ($submarca_gs) {
 
                 $modelo_gs = $this->searchModelos($submarca_gs, $modelo);
-                dd($submarca_gs,$modelo_gs,$marca_gs);
+                // dd($submarca_gs,$modelo_gs,$marca_gs);
                 if ($modelo_gs) {
                     $versiones_gs = $this->searchVersiones($submarca_gs, $modelo_gs);
-                    // dd($versiones_gs->amis);
+                    dd($versiones_gs);
                     // dd(count(array($versiones_gs)));
                     if (isset($versiones_gs->amis)) {
                         // $aux = array( 
