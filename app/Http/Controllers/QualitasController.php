2bb66a1aa09ea802c2873d90ced15729e37f9648
MarcoAMtz99 <marcoamtz99@gmail.com>
2021-09-03 17:53:10 -0500

    Cache

--- app/Http/Controllers/QualitasController.php
+++ app/Http/Controllers/QualitasController.php
@@ -110,7 +110,7 @@ class QualitasController extends Controller
 		$xml = simplexml_load_string($result->listaTarifasResult->any);
 		$results = json_decode(json_encode($xml), true);
 		$descripciones= [];
-			dd($results);
+			dd($results,$modelo,$marca);
 		foreach ($results['datos'] as $key => $value) {
 			foreach ($value as $key => $submarcas) {	
 				dd($submarcas);
