df899beb17e04b73c856323328e853ca69c48e8f
MarcoAMtz99 <marcoamtz99@gmail.com>
2021-09-02 23:32:40 -0500

    Cambios en pruebaQa

--- app/Http/Controllers/QualitasController.php
+++ app/Http/Controllers/QualitasController.php
@@ -85,7 +85,7 @@ class QualitasController extends Controller
 		foreach ($results as $key => $value) {
 			
 			foreach ($value as $key => $submarcas) {	
-			
+				dd($submarcas);
 				array_push($descripciones, $submarcas['cTipo']);
 			}
 			
