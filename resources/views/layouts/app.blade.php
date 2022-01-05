7907c06657b0c46b224d406235e9c541feb80e4c
caros182 <caros182@gmail.com>
2022-01-05 14:19:16 -0600

    Update app.blade.php

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -188,18 +188,18 @@
             </a>-->
             <div class="col-12 col-sm-3">
                 <p class="font-weight-bold text-center">Encuéntranos en redes sociales</p>
-                <p class="text-center"><a href="https://www.facebook.com/autosegurodirecto" target="_blank"><i class="fab fa-facebook-square" style="font-size:36px; color:#000; margin:5px;"></i></a><a href="https://twitter.com/autosegurodirecto" target="_blank"><i class="fab fa-twitter-square" style="font-size:36px; color:#000; margin:5px"></i></a></p>
+                <p class="text-center"><a href="https://www.facebook.com/autosegurodirecto" target="_blank"><i class="fab fa-facebook-square f-social"></i></a><a href="https://twitter.com/autosegurodirecto" target="_blank"><i class="fab fa-twitter-square f-social " ></i></a></p>
             </div>
-            <div class="col-12 col-sm-3">
+            <div class="col-12 col-sm-2">
                 <p class="font-weight-bold text-center"></p>
                 <p class="text-center"></p>
             </div>
-            <div class="col-12 col-sm-3">
+            <div class="col-12 col-sm-4">
                 <p class="font-weight-bold text-center">
                   ¿Necesitas asistencia?
                 </p>
-                <p class="text-center"><i class="material-icons" style="font-size:30px; color:#ffdd00;">call</i>(55) 6275-8686</p>
-                <p class="text-center"><i class="material-icons" style="font-size:30px; color:#ffdd00;">mail</i><a href="mailto:contacto@autosegurodirecto.com">contacto@autosegurodirecto.com</a></p>
+                <p class="text-center"><i class="material-icons">call</i>(55) 6275-8686</p>
+                <p class="text-center"><i class="material-icons">mail</i><a href="mailto:contacto@autosegurodirecto.com">contacto@autosegurodirecto.com</a></p>
             </div>
             <div class="col-12 col-sm-3">
                 <p class="font-weight-bold text-center">Legales</p>
@@ -212,7 +212,12 @@
                     </a> --}}
                 </p>-->
             </div>
+            <br>
+            <div class="col-12 col-sm-2"></div>
+            <div class="col-12 col-sm-7"><p class="cop">© 2021 CooperturaAgente de Seguros y Fianzas. Todos los derechos reservados.</p></div>
+            <div class="col-12 col-sm-2"></div>
         </div>
+
 
     </body>
                 <script type="text/javascript" src="http://code.jquery.com/jquery-1.11.0.min.js"></script>
